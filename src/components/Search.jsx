db6d6ccc30e0fae2844c837533f95485910fef3f
Ehab <ehabalhab7@gmail.com>
2026-09-05 20:36:26 +0300

    first commit

---
+++ src/components/Search.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Search({ searchTerm, setSearchTerm }) {
+    return (
+        <div className='search'>
+            <div>
+                <img src="../public/search.svg" alt="search" />
+                <input type="text"
+                 placeholder="search for a movie"
+                 value={searchTerm}
+                 onChange={(event) => setSearchTerm(event.target.value)} />
+
+            </div>
+        </div>
+    )
+}
+
+export default Search
